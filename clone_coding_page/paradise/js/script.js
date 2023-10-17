3de5d04b6c7bd44a5d1391691637d0bde3667efc
sangwoolee3003 <148169110+sangwoolee3003@users.noreply.github.com>
2023-10-17 11:15:45 +0900

    Add files via upload

---
+++ clone_coding_page/paradise/js/script.js
@@ -0,0 +1,18 @@
+// 상단 슬라이드
+$(function(){
+    $('.slide').slick({
+        infinite: true,
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:1000,
+        fade: true,
+
+
+        // centerPadding: '0px',
+    });
+
+});
+
+
+
+
